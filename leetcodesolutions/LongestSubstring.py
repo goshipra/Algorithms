e58c963ded2979e52d98f2fadfd0bca1c6da020a
Shipra Rathore <shipra.rathore17@gmail.com>
2022-06-06 16:40:23 -0700

    finding LongestSubstring.py

---
+++ leetcodesolutions/LongestSubstring.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# LongestSubstring.py
+# Author : Shipra
+
+class Solution(object):
+    '''
+    Given a string s, find the length of the longest substring without
+    repeating characters.
+    '''
+    def lengthOfLongestSubstring(self, s):
+        listSubstring = []
+        lengthSubstring = []
+
+        if len(s) < 2:
+            if len(s) == 0:
+                return 0
+            else:
+                return len(s)
+
+        outputString = s[0]
+        if len(s) == 2:
+            if outputString == s[1]:
+                return len(outputString)
+            else:
+                outputString = outputString + s[1]
+                return len(outputString)
+
+        for i in range(1, len(s)):
+            if s[i] in outputString:
+
+                # Work on this part
+                if s[i] != s[i-1]:
+                    previous_index = outputString.index(s[i])
+                    oldSubstring = outputString
+                    outputString = s[previous_index:] + s[i]
+                    listSubstring.append(oldSubstring)
+                    print(outputString)
+                    print(listSubstring)
+                else:
+                    oldSubstring = outputString
+                    outputString = s[i]
+                    listSubstring.append(oldSubstring)
+                    print(outputString)
+                    print(listSubstring)
+            else:
+                outputString = outputString + s[i]
+                listSubstring.append(outputString)
+                print(outputString)
+                print(listSubstring)
+
+        print(listSubstring)
+        for item in listSubstring:
+            lengthSubstring.append(len(item))
+
+        maximum_length = max(lengthSubstring)
+        return maximum_length
+
+
+s = 'ckilbkd'
+obj = Solution()
+print(obj.lengthOfLongestSubstring(s))
