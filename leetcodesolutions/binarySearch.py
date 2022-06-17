7c05aa20c44bfb2c20fcc9aacd2f3848aabbd7ae
Shipra Rathore <shipra.rathore17@gmail.com>
2022-06-17 16:02:40 -0700

    Binary search tree

---
+++ leetcodesolutions/binarySearch.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# binarySearch.py
+# Author : Shipra
+
+class Solution:
+    def search(self, nums, target):
+        '''
+        Binary search tree
+        '''
+        if not nums:
+            return -1
+
+        low = 0
+        high = len(nums) - 1
+        mid = int(high / 2)
+
+        if target == nums[mid]:
+            return mid
+
+        while low <= high:
+            mid = int((low + high) / 2)
+            if target > nums[mid]:
+                low = mid + 1
+            elif target < nums[mid]:
+                high = mid - 1
+            elif target == nums[mid]:
+                return mid
+
+        return -1
+
+
+nums = [90,90]
+target = 90
+obj = Solution()
+print(obj.search(nums, target))
