bc230191f49ab8d84e9bce2d202e33faf9b0f09b
Shipra Rathore <shipra.rathore17@gmail.com>
2025-01-28 10:51:12 -0800

    instertion sort

--- commaoncodingquestions/Revision/sortingandsearching.py
+++ commaoncodingquestions/Revision/sortingandsearching.py
@@ -3,11 +3,7 @@
 # Write a binary search function.
 # Merge two sorted lists into one sorted list.
 
-
 def bubble_sort(Array):
-    # if num[i] > num[i+1],
-    #  then swap and i = i+1, 
-    # else: i = i+1;
     lenth = len(Array)
     for j in range(lenth-1):
         for i in range(lenth-1):
@@ -18,4 +14,24 @@ def bubble_sort(Array):
     print(Array)
 
 my_list = [0,9,5,3,2,4,6,9]
-bubble_sort(my_list)
+# bubble_sort(my_list)
+
+
+
+def insertion_sort(mylist):
+    # first loop : second element to last, 
+    # inner loops : n-1 till element is greater
+    #  than that element. 
+    for i in range(1,len(my_list)):
+        for j in range(i-1,-1,-1):
+            print(my_list)
+            if my_list[i] <= my_list[j]:
+                my_list[i],my_list[j] = my_list[j],my_list[i]
+                i = i -1
+            else:
+                continue    
+
+    print(my_list)
+
+insertion_sort(my_list)
+
